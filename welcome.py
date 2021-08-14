3c5ef3072099fbf0e4f86edd16a76def0d1272b1
sai tarun <saitarunsan@gmail.com>
2021-08-14 14:12:43 +0530

    few changes in printing

--- welcome.py
+++ welcome.py
@@ -1,9 +1,9 @@
 input = input('enter the data:')
 def func1(input):
     if len(input)>1:
-        print('valid input')
+        print('valid input',input)
     else:
-        print('invalid input')
+        print('invalid input',input)
 
 func1(input)
 
